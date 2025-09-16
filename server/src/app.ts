d763b35b217282fc45fde4019bd2d0a3db8c845e
Hui Liu <hliu121@students.cs.ubc.ca>
2025-09-16 08:01:33 -0700

    auth route

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,17 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+import authRoutes from "./routes/auth.route";
+
 dotenv.config();
 const app = express();
+console.log('MONGO_URI:', process.env.MONGO_URI);
+console.log('PORT:', process.env.PORT);
+
 app.use(cors());
-app.get('/api/health', (req, res) => {
-    res.json({status: 'ok', message:'RenalFlow server running'})
-});
+app.use(express.json()); 
+
+app.use("/api/auth", authRoutes);
+
 
 export default app;

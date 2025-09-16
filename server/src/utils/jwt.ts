41b820aa7b7e3f580fe8b609072b45e4337352ba
Hui Liu <hliu121@students.cs.ubc.ca>
2025-09-16 13:47:28 -0700

    backend refreshtoken change

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -1,13 +1,10 @@
 import jwt from 'jsonwebtoken';
 import { Types } from 'mongoose';
-
-export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || '';
-export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || '';
+import { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } from '../config/env';
 
 export const generateAccessToken = (userId: Types.ObjectId) => {
     return jwt.sign({ userId: userId.toString() }, ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
 };
-
 export const generateRefreshToken = (userId: Types.ObjectId) => {
     return jwt.sign({ userId: userId.toString() }, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
 };
